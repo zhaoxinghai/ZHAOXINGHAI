c4d91a680f9b298de782c3b8ac279712dbf71773
zhaoxinghai <441666391@qq.com>
2018-04-25 15:52:50 +0800

    Add project

---
+++ Console/source/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test");
+        }
+    }
+}
---
+++ Web/MyCMS.Web/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyCMS.Web.Models;
+
+namespace MyCMS.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public int test = 0;
+        public IActionResult Index()
+        {
+            test++;
+
+            int a = 3 + 4;
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            test++;
+            int a = 3 + 4;
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
