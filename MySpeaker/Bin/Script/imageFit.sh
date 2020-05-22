2062fc62f7df2d1b5c70d6299b850e58dad95d47
zhaoxinghai <441666391@qq.com>
2020-05-22 09:54:45 +0800

    +

--- MySpeaker/Bin/Script/imageFit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -e $HIDE_DIR/Script/image.fit ]; then
-	echo "image.fit is upgrading..."
-
-	flash_erase /dev/mtd8 0 0
-	nandwrite -p /dev/mtd8 $HIDE_DIR/Script/image.fit
-	export RebootFlag=1
-fi
--- MySpeaker/Manager/Include/constant.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONSTANT_H
-#define CONSTANT_H
-
-#define RESET_PASSWD_NUM        "2352385087"
-#define RESET_ACCOUNT_USER      "admin"
-
-
-
-#endif // CONSTANT_H
